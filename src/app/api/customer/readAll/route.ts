4e070339e8bda90adb5af822479a4d66963f0ab1
Maheen Akhtar Khan <116183925+mkhan429419@users.noreply.github.com>
2024-08-25 15:13:19 +0400

    readAll route added

---
+++ src/app/api/customer/readAll/route.ts
@@ -0,0 +1,29 @@
+import { NextResponse } from 'next/server';
+import { ScanCommand, DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb';
+import { dynamoDBClient } from '../../../../../awsConfig';
+
+const documentClient = DynamoDBDocumentClient.from(dynamoDBClient);
+
+export async function GET() {
+  try {
+    const params = {
+      TableName: 'PamVoiceAgent',
+    };
+
+    const data = await documentClient.send(new ScanCommand(params));
+
+    return NextResponse.json(
+      {
+        message: 'Customer profiles retrieved successfully',
+        data: data.Items,
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error('Error retrieving customer profiles:', error);
+    return NextResponse.json(
+      { error: 'Could not retrieve customer profiles' },
+      { status: 500 }
+    );
+  }
+}
