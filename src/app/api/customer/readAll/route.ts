e0e93d1ceb993e0d54b9305ce75d55d0ff25242c
Maheen Akhtar Khan <116183925+mkhan429419@users.noreply.github.com>
2024-08-26 16:51:33 +0400

    readAll route update

--- src/app/api/customer/readAll/route.ts
+++ src/app/api/customer/readAll/route.ts
@@ -1,28 +1,29 @@
-import { NextResponse } from 'next/server';
-import { ScanCommand, DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb';
-import { dynamoDBClient } from '../../../../../awsConfig';
+import { NextResponse } from "next/server";
+import { ScanCommand, DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
+import { dynamoDBClient } from "../../../../../awsConfig";
 
 const documentClient = DynamoDBDocumentClient.from(dynamoDBClient);
 
 export async function GET() {
   try {
     const params = {
-      TableName: 'PamVoiceAgent',
+      TableName: "PamVoiceAgent",
+      ConsistentRead: true, // Force strong consistency
     };
 
     const data = await documentClient.send(new ScanCommand(params));
 
     return NextResponse.json(
       {
-        message: 'Customer profiles retrieved successfully',
+        message: "Customer profiles retrieved successfully",
         data: data.Items,
       },
       { status: 200 }
     );
   } catch (error) {
-    console.error('Error retrieving customer profiles:', error);
+    console.error("Error retrieving customer profiles:", error);
     return NextResponse.json(
-      { error: 'Could not retrieve customer profiles' },
+      { error: "Could not retrieve customer profiles" },
       { status: 500 }
     );
   }
